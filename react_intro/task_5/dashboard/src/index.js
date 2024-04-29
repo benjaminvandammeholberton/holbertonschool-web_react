f2e6371fe7502ffd8b6718c03bb2cbecf43c3978
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-04-29 15:53:01 +0200

    fix: task 8

--- react_intro/task_5/dashboard/src/index.js
+++ react_intro/task_5/dashboard/src/index.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Notifications from './Notifications/Notifications';
+import App from './App/App';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+const rootNotifications = ReactDOM.createRoot(
+  document.getElementById('root-notifications')
+);
+rootNotifications.render(
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>
+);
