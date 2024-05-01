c6b71ce433b8aa225d97deccefcfa6c61b935f72
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-05-01 16:19:35 +0200

    fix: task 1

--- React_component/task_1/dashboard/src/App/App.js
+++ React_component/task_1/dashboard/src/App/App.js
@@ -8,13 +8,13 @@ import CourseList from '../CourseList/CourseList';
 import './App.css';
 import { getLatestNotifications } from '../utils/utils';
 
-const coursesData = [
+const listCourses = [
   { id: 1, name: 'ES6', credit: 60 },
   { id: 2, name: 'Webpack', credit: 20 },
   { id: 3, name: 'React', credit: 40 },
 ];
 
-const notificationData = [
+const listNotifications = [
   { id: 1, type: 'default', value: 'New course available' },
   { id: 2, type: 'urgent', value: 'New resume available' },
   { id: 3, type: 'urgent', html: { __html: getLatestNotifications() } },
