d038261a13caf010664c1a01b3c377ecaa9e3ff0
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-05-01 16:16:21 +0200

    fix: task 1

--- React_component/task_1/dashboard/src/App/App.js
+++ React_component/task_1/dashboard/src/App/App.js
@@ -1,52 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import logo from '../assets/holberton_logo.jpg';
-import { getFullYear, getFooterCopy } from '../utils/utils';
 import Notifications from '../Notifications/Notifications';
-import { getLatestNotification } from '../utils/utils';
-import Login from '../Login/Login';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
 import CourseList from '../CourseList/CourseList';
 import './App.css';
+import { getLatestNotifications } from '../utils/utils';
 
-const listCourses = [
-  {
-    id: 1,
-    name: 'ES6',
-    credit: 60,
-  },
-  {
-    id: 2,
-    name: 'Webpack',
-    credit: 20,
-  },
-  {
-    id: 3,
-    name: 'React',
-    credit: 40,
-  },
+const coursesData = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 },
 ];
 
-const listNotifications = [
-  {
-    id: 1,
-    type: 'default',
-    value: 'New course available',
-  },
-  {
-    id: 2,
-    type: 'urgent',
-    value: 'New resume available',
-  },
-  {
-    id: 3,
-    type: 'urgent',
-    html: { __html: getLatestNotification() },
-  },
+const notificationData = [
+  { id: 1, type: 'default', value: 'New course available' },
+  { id: 2, type: 'urgent', value: 'New resume available' },
+  { id: 3, type: 'urgent', html: { __html: getLatestNotifications() } },
 ];
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.handleKey = this.handleKey.bind(this);
@@ -61,6 +35,7 @@ class App extends React.Component {
       this.props.logOut();
     }
   }
+
   componentDidMount() {
     window.addEventListener('keydown', this.handleKey);
   }
@@ -70,22 +45,15 @@ class App extends React.Component {
   }
 
   render() {
-    const footerText = `Copyright ${getFullYear()} - ${getFooterCopy(true)}`;
-    let body;
-
-    if (this.props.isLoggedIn) {
-      body = <CourseList listCourses={listCourses} />;
-    } else {
-      body = <Login text="Login to access the full dashboard" />;
-    }
     return (
       <>
-        <Notifications listNotifications={listNotifications} />
+        <Notifications />
         <div className="App">
-          <Header text="School dashboard" src={logo} alt="Holberton logo" />
-          <div className="App-body">{body}</div>
-          <Footer text={footerText} />
+          <Header />
+          {!this.props.isLoggedIn ? <Login /> : <CourseList />}
+          <Footer />
         </div>
+        ;
       </>
     );
   }
@@ -93,7 +61,6 @@ class App extends React.Component {
 
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
-  logOut: PropTypes.func,
 };
 
 App.defaultProps = {
