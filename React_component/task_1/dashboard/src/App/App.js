d720e1a1d42c3ba9bc5306a07aae21f13ce3dbd1
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-05-01 16:23:26 +0200

    fix: task 1

--- React_component/task_1/dashboard/src/App/App.js
+++ React_component/task_1/dashboard/src/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Notifications from '../Notifications/Notifications';
 import Header from '../Header/Header';
@@ -8,19 +8,19 @@ import CourseList from '../CourseList/CourseList';
 import './App.css';
 import { getLatestNotifications } from '../utils/utils';
 
-const listCourses = [
+const coursesData = [
   { id: 1, name: 'ES6', credit: 60 },
   { id: 2, name: 'Webpack', credit: 20 },
   { id: 3, name: 'React', credit: 40 },
 ];
 
-const listNotifications = [
+const notificationData = [
   { id: 1, type: 'default', value: 'New course available' },
   { id: 2, type: 'urgent', value: 'New resume available' },
   { id: 3, type: 'urgent', html: { __html: getLatestNotifications() } },
 ];
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.handleKey = this.handleKey.bind(this);
@@ -45,22 +45,15 @@ class App extends React.Component {
   }
 
   render() {
-    const footerText = `Copyright ${getFullYear()} - ${getFooterCopy(true)}`;
-    let body;
-
-    if (this.props.isLoggedIn) {
-      body = <CourseList listCourses={listCourses} />;
-    } else {
-      body = <Login text="Login to access the full dashboard" />;
-    }
     return (
       <>
-        <Notifications listNotifications={listNotifications} />
+        <Notifications />
         <div className="App">
-          <Header text="School dashboard" src={logo} alt="Holberton logo" />
-          <div className="App-body">{body}</div>
-          <Footer text={footerText} />
+          <Header />
+          {!this.props.isLoggedIn ? <Login /> : <CourseList />}
+          <Footer />
         </div>
+        ;
       </>
     );
   }
@@ -68,6 +61,7 @@ class App extends React.Component {
 
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
 };
 
 App.defaultProps = {
